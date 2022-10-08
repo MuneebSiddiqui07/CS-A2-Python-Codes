6b1d537080a199368c8af700bf324cba3097fd30
MuneebSiddiqui07 <77151090+MuneebSiddiqui07@users.noreply.github.com>
2022-10-07 21:49:33 -0700

    Update Binary Search.py

--- Binary Search.py
+++ Binary Search.py
@@ -1,21 +1,25 @@
 alpha = ['a','b','c','d','e','f','g','h','i','j']
 
 def BinaryS(value):
-    LB = 1
-    UB = 10
-    isfound=false
-    mid= [(UB+LB)/2]
-    while UB>=LB or isfound = false :
-        if value = alpha [mid]:
-           isfound = true
-        elif value > alpha [mid]:
-               LB = mid +1
-        elif value < alpha [mid]:
-              UB = mid - 1
-
-    x = input ("Enter an alphabet: ")
-    if isfound = true:
-        print ("The alphabet", x "is found at", BinaryS(x))
-    elif print ("The alphabet", x "is not present in the array")
-
-
+    LB = 0
+    UB = 9
+    isfound = False
+    while UB>=LB and isfound == False:
+        mid = (UB+LB)//2
+        if value == alpha[mid]:
+           isfound = True
+        elif value > alpha[mid]:
+            LB = mid +1
+        elif value < alpha[mid]:
+            UB = mid - 1
+    if isfound == True: 
+        return mid 
+    else:
+        return -1
+    
+x = input ("Enter an alphabet: ")
+answer = BinaryS(x)
+if x != -1:
+    print ("The alphabet", x, "is found at", answer)
+else:
+    print ("The alphabet", x, "is not present in the array")
