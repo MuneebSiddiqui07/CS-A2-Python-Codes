4b58b9207d0fb8237b298d31b9c383dce58e8977
MuneebSiddiqui07 <77151090+MuneebSiddiqui07@users.noreply.github.com>
2022-10-11 07:39:23 +0500

    Update Recursive Binary Search.py

--- Recursive Binary Search.py
+++ Recursive Binary Search.py
@@ -1,21 +1,21 @@
-def RecursiveBS(UB, LB, Val):
+def RecursiveBS(Array, UB, LB, Val):
     mid = int((UB + LB) / 2)
     if LB > UB:
         return -1
     else:
-        if Val == SearchArray[mid]:
+        if Val == Array[mid]:
             return mid
-        elif SearchArray[mid] < Val:
-            return RecursiveBS(UB, mid + 1, Val)
-        elif SearchArray[mid] > Val:
-            return RecursiveBS(mid - 1, LB, Val)
+        elif Array[mid] < Val:
+            return RecursiveBS(Array, UB, mid + 1, Val)
+        elif Array[mid] > Val:
+            return RecursiveBS(Array, mid - 1, LB, Val)
 
-SearchArray = ["a", "b", "c", "d"]
+Array = ["a", "b", "c", "d"]
 LB = 0
 UB = 3
 Val = input("Enter a value to search: ")
-answer = RecursiveBS(3, 0, Val)
-if answer == -1:
+ans = RecursiveBS(Array, 3, 0, Val)
+if ans == -1:
     print(Val, "not found in array")
 else:
-    print(Val, "Found at location: ", answer)
+    print(Val, "Found at location: ", ans)
