7717af4df1fc9490e7e19be4bdf1e1e1b15ba617
Muneeb Siddiqui <77151090+MuneebSiddiqui07@users.noreply.github.com>
2022-10-22 07:00:20 +0500

    Delete Factorial.py

--- Factorial.py
+++
@@ -1,8 +0,0 @@
-def factorial(num):
-    f=1
-    for i in range(num,0,-1):
-        f= f*i
-    return f
-x = int(input("Enter number whose factorial you would like to find: "))
-answer = factorial(x)
-print("Your answer is: ",answer)
