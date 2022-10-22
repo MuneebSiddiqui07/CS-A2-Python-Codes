b9643a7a1fa48acc11901c25643b5b8ccb16f0ca
Muneeb Siddiqui <77151090+MuneebSiddiqui07@users.noreply.github.com>
2022-10-22 07:00:41 +0500

    Delete Recursive Factorial.py

--- Recursive Factorial.py
+++
@@ -1,8 +0,0 @@
-def RecursiveFactorial(num):
-    if num == 1 or num == 0:
-        return 1
-    else:
-        return num*RecursiveFactorial(num - 1)
-num = int(input("Enter a number: "))
-answer = RecursiveFactorial(num)
-print("Your answer is: ", answer)
